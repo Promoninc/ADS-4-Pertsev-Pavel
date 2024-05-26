f4986e737bb55ae8d7d87435854942597505385e
Promoninc <promoninc@gmail.com>
2024-05-26 15:17:01 +0500

    Full implementation of assignment

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,19 +1,28 @@
-import java.util.Map;
+import java.util.*;
 
-public class Vertex<T> {
-    private T data;
-    private Map<Vertex<T>, Double> adjacentVertices;
-    public Vertex(T data){
+public class Vertex<V> {
+    private V data;
+    private Map<Vertex<V>, Double> adjacentVertices; 
+
+    public Vertex(V data) {
         this.data = data;
-    }
-    public void addAdjacent(Vertex<T> vertex, Double weight){
-        adjacentVertices.put(vertex, weight);
-    }
-    public double getWeight(Vertex<T> vertex){
-        return adjacentVertices.get(vertex);
-    }
-    public void removeAdjacent(Vertex<T> vertex){
-        adjacentVertices.remove(vertex);
+        this.adjacentVertices = new HashMap<>();
     }
 
+    public V getData() {
+        return data;
+    }
+
+    public void addAdjacent(Vertex<V> destination, double weight) {
+        adjacentVertices.put(destination, weight);
+    }
+
+    public Map<Vertex<V>, Double> getAdjacent() {
+        return adjacentVertices;
+    }
+
+    @Override
+    public String toString() {
+        return data.toString();
+    }
 }
